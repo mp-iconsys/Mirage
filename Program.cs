51d44f5ff1583c55862f01f9d1dcb15e9e45fc5c
mp-iconsys <mikolaj.paliszewski@iconsys.co.uk>
2020-05-27 12:57:20 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Mirage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
